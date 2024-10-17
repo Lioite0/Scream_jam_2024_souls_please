e1023939ac87cf2e51ff411f1d90fbf1f870e985
Lioite0 <128874727+Lioite0@users.noreply.github.com>
2024-10-17 14:41:00 -0700

    Update cam_movement.cs

--- souls_please/Assets/cam_movement.cs
+++ souls_please/Assets/cam_movement.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class cam_movement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float sensitivity = 2f;
+    float cameraVertRotation = 0f;
+    float cameraHorizRotation;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputX = Input.GetAxis("Mouse X") * sensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * sensitivity;
+
+    cameraVertRotation -= inputY;
+    cameraVertRotation = Mathf.Clamp(cameraVertRotation, -90f, 90f);
+    
+    cameraHorizRotation += inputX;
+    transform.rotation = Quaternion.Euler(cameraVertRotation, cameraHorizRotation, 0);
     }
 }
